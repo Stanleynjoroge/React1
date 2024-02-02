b4457d5b0b29f60cf6563fced7791c3bc693422f
Stanleynjoroge <ngangastanley903@gmail.com>
2024-02-02 23:22:41 +0300

    ReactRouters

---
+++ ReactRouterDom/src/main.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// import * as React from "react"
+// import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './routes/root.jsx';
+import Contact ,{loader as contactLoader,} from './routes/contacts.jsx';
+import  { loader as rootLoader,action as rootAction, } from "./routes/root";
+import ErrorPage from './routes/ErrorPage.jsx';
+import EditContact,{action as editAction,} from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+const router = createBrowserRouter([
+  {
+    path: "/",element: <Root/>,
+    errorElement: <ErrorPage/> ,
+    loader: rootLoader,
+    action: rootAction,
+    children:[
+      {
+        path: "/contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path:"/signup",
+        element:<App/>
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+          },
+    ]
+  },
+ 
+  
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  
+  </React.StrictMode>,
+)
